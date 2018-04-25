39eafab53bbfb7c954b153910da45f3694025991
Stephan <stepannk@mail.ru>
2018-04-25 09:57:11 +0300

    Replace web.xml with annotations

--- src/main/java/ru/zuma/LogServlet.java
+++ src/main/java/ru/zuma/LogServlet.java
@@ -4,12 +4,14 @@ import ru.zuma.data.RPILogManager;
 import ru.zuma.utils.JSONConverter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 
+@WebServlet("/log")
 public class LogServlet extends HttpServlet {
 
     @Override
--- src/main/java/ru/zuma/PostLogServlet.java
+++ src/main/java/ru/zuma/PostLogServlet.java
@@ -4,12 +4,14 @@ import ru.zuma.data.RPILogManager;
 import ru.zuma.utils.JSONConverter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+@WebServlet("/postlog")
 public class PostLogServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
